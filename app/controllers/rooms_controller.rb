7b800ce550114f3062abc9b0997c2f4eee6c862f
Asher Trockman <asher.trockman@gmail.com>
2017-09-29 18:56:26 -0500

    rooms now show themselves

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,4 +1,20 @@
 class RoomsController < ApplicationController
 	def new
 	end
+
+	def show
+		@room = Room.find(params[:id])
+	end
+
+	def create
+		@room = Room.new(room_params)
+
+		@room.save
+		redirect_to @room
+	end
+
+	private
+		def room_params
+			params.require(:room).permit(:name)
+		end
 end
