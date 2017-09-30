565dc78edc33e4a531ad26fa789ce5692b0cea9d
Asher Trockman <asher.trockman@gmail.com>
2017-09-29 21:33:48 -0500

    delete questions

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -5,6 +5,13 @@ class QuestionsController < ApplicationController
 		redirect_to room_path(@room)
 	end
 
+	def destroy
+		@room = Room.find(params[:room_id])
+		@question = @room.questions.find(params[:id])
+		@question.destroy
+		redirect_to room_path(@room)
+	end
+
 	private
 		def question_params
 			params.require(:question).permit(:body)
