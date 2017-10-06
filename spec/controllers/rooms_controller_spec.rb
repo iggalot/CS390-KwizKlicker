91f6d2c8e04b71956f32d6deb2871789887e4fc8
Asher Trockman <asher.trockman@gmail.com>
2017-10-06 12:19:25 -0500

    controller tests

--- spec/controllers/rooms_controller_spec.rb
+++ spec/controllers/rooms_controller_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe RoomsController, type: :controller do
+	describe 'POST #create' do
+		it 'makes a new room and redirects to it' do
 
+			post :create, params: {:room => {:name => 'test room more chars'}}
+			expect(response).to redirect_to '/rooms/1'
+
+			post :create, params: {:room => {:name => 'test room more chars'}}
+			expect(response).to redirect_to '/rooms/2'
+		end
+	end
 end
