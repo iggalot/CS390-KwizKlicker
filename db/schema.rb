65cb3681e440a7a388ad4fde8f69b81671ff267c
Asher Trockman <asher.trockman@gmail.com>
2017-10-13 14:40:36 -0500

    add password field to room [with Dalton]

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170930002340) do
+ActiveRecord::Schema.define(version: 20171013193806) do
 
   create_table "questions", force: :cascade do |t|
     t.text "body"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 20170930002340) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password"
   end
 
 end
