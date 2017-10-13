107770a0f9e9dca9f1a3657d5a50c625a76d5fe6
Asher Trockman <asher.trockman@gmail.com>
2017-10-13 14:44:31 -0500

    password validation [with Dalton]

--- app/models/room.rb
+++ app/models/room.rb
@@ -2,4 +2,6 @@ class Room < ApplicationRecord
 	has_many :questions
 	validates :name, presence: true,
 					 length: { minimum: 10 }
+	validates :password, presence: true,
+					 length: { minimum: 2 }
 end
