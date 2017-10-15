c3ca90770a752d76f5838ced42f9e6cd2aa00936
Asher Trockman <asher.trockman@gmail.com>
2017-10-15 18:21:01 -0500

    add an authentication form if not authed

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -17,6 +17,23 @@ class RoomsController < ApplicationController
 		return session[:rooms].include? params[:id].to_i
 	end
 
+	def auth
+		@room = Room.find(params[:id])
+		
+		if params[:password] == @room.password
+			if not session.has_key?(:rooms)
+				session[:rooms] = []
+			end
+
+			session[:rooms].push(@room.id.to_i)
+			redirect_to @room
+		else
+			flash[:auth] = "Wrong password"
+			render 'unauthed'
+		end
+	end
+		
+
 	def create
 		@room = Room.new(room_params.merge(:password=>"password123"))
 		if not session.has_key?(:rooms)
