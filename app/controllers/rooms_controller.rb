de72621ea5a09ed7f6532830e8fe987a7ab56d24
Asher Trockman <asher.trockman@gmail.com>
2017-10-15 17:48:56 -0500

    add sessions for authentication, default room password, view for unauthorized room

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -5,11 +5,27 @@ class RoomsController < ApplicationController
 
 	def show
 		@room = Room.find(params[:id])
+
+		if authed?
+			render 'show'
+		else
+			render 'unauthed'
+		end
+	end
+
+	def authed?
+		return session[:rooms].include? params[:id].to_i
 	end
 
 	def create
-		@room = Room.new(room_params)
+		@room = Room.new(room_params.merge(:password=>"password123"))
+		if not session.has_key?(:rooms)
+			session[:rooms] = []
+		end
 
+		# TODO better way
+		session[:rooms].push(Room.last.id + 1)
+			
 		if @room.save
 			redirect_to @room
 		else
