9a519b21e049a026636c0eb27c309e0d291c9add
Asher Trockman <asher.trockman@gmail.com>
2017-10-15 18:44:58 -0500

    tests that can run in any order

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -3,10 +3,10 @@ require 'rails_helper'
 RSpec.describe QuestionsController, type: :controller do
 	describe "POST #create" do
 		before { Room.create(name: "a good room i guess") }
-
+		id = Room.last.id
 		it 'puts a question in a room' do
-			post :create, :params => {:question => {:body => "a question"}, :room_id => 1}
-			expect(response).to redirect_to '/rooms/1'
+			post :create, :params => {:question => {:body => "a question"}, :room_id => id}
+			expect(response).to redirect_to '/rooms/' + id
 		end
 	end
 end
--- spec/controllers/rooms_controller_spec.rb
+++ spec/controllers/rooms_controller_spec.rb
@@ -3,12 +3,11 @@ require 'rails_helper'
 RSpec.describe RoomsController, type: :controller do
 	describe 'POST #create' do
 		it 'makes a new room and redirects to it' do
+			post :create, params: {:room => {:name => 'test room more chars'}}
+			expect(response).to redirect_to '/rooms/' + Room.last.id
 
 			post :create, params: {:room => {:name => 'test room more chars'}}
-			expect(response).to redirect_to '/rooms/1'
-
-			post :create, params: {:room => {:name => 'test room more chars'}}
-			expect(response).to redirect_to '/rooms/2'
+			expect(response).to redirect_to '/rooms/' + Room.last.id
 		end
 	end
 end
