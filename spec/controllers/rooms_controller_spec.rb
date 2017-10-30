bdaca195fbda92fff8ee16e672df2c6201de1d94
Don Roberts <roberts@refactory.com>
2017-10-30 14:30:28 -0500

    Added rspec test to ensure that number of rooms changes on creation.

--- spec/controllers/rooms_controller_spec.rb
+++ spec/controllers/rooms_controller_spec.rb
@@ -3,11 +3,13 @@ require 'rails_helper'
 RSpec.describe RoomsController, type: :controller do
 	describe 'POST #create' do
 		it 'makes a new room and redirects to it' do
-			post :create, params: {:room => {:name => 'test room more chars'}}
-			expect(response).to redirect_to '/rooms/' + Room.last.id.to_s
+      expect {
+				post :create, params: {:room => {:name => 'test room more chars'}}
+				expect(response).to redirect_to '/rooms/' + Room.last.id.to_s
 
-			post :create, params: {:room => {:name => 'test room more chars'}}
-			expect(response).to redirect_to '/rooms/' + Room.last.id.to_s
+				post :create, params: {:room => {:name => 'test room more chars'}}
+				expect(response).to redirect_to '/rooms/' + Room.last.id.to_s
+      }.to change{Room.all.count}.by(2)
 		end
   end
 
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -46,6 +46,19 @@ RSpec.configure do |config|
   # triggering implicit auto-inclusion in groups with matching metadata.
   config.shared_context_metadata_behavior = :apply_to_host_groups
 
+  config.before(:suite) do
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.start
+  end
+
+  config.append_after(:each) do
+    DatabaseCleaner.clean
+  end
+
 # The settings below are suggested to provide a good initial experience
 # with RSpec, but feel free to customize to your heart's content.
 =begin
