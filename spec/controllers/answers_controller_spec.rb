d476ede7da3b0707ff371c3c23063c3eb46f20a2
Asher Trockman <asher.trockman@gmail.com>
2017-11-03 14:37:25 -0500

    make tests actually test things

--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe AnswersController, type: :controller do
+    before(:all) do
+        Room.create(name: "testing this room", password: "passwords", roomcode: "aaaA")
+        @room_id = Room.last.id
+        Question.create(body: "the question here", room_id: @room_id)
+        @question_id = Question.last.id
+    end
 
+    describe "Teacher makes a question, then adds answers to that question" do
+        it 'adds a question' do
+            expect {
+                post :create, :params => {:answer => {:text => "an answer"}, :room_id => @room_id, :question_id => @question_id }
+                expect(response).to redirect_to '/rooms/' + @room_id.to_s + '/questions/' + @question_id.to_s
+            }.to change{Answer.all.count}.by(1)
+        end
+    end
 end
--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -3,12 +3,14 @@ require 'rails_helper'
 RSpec.describe QuestionsController, type: :controller do
 	describe "POST #create" do
 		before(:all) do
-			Room.create(name: "a good room i guess", password: "passwords", roomcode:"aaaa")
+			Room.create(name: "a good room i guess", password: "passwords", roomcode: "aaaa")
 			@id = Room.last.id
 		end
 		it 'puts a question in a room' do
-			post :create, :params => {:question => {:body => "a question"}, :room_id => @id}
-			expect(response).to redirect_to '/rooms/' + @id.to_s
+            expect {
+                post :create, :params => {:question => {:body => "a question"}, :room_id => @id}
+                expect(response).to redirect_to '/rooms/' + @id.to_s
+            }.to change{Question.all.count}.by(1)
 		end
 	end
 end
