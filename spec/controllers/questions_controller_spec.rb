8b93f5b6edf7c0d23693b14794a60abec5a9cf2a
Asher Trockman <asher.trockman@gmail.com>
2017-11-06 14:27:09 -0600

    add test for deletion of question

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -1,11 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe QuestionsController, type: :controller do
+    before(:all) do
+        Room.create(name: "a good room i guess", password: "passwords", roomcode: "aaaa")
+        @room = Room.last
+        @id = Room.last.id
+        Question.create(body: "a new question", room_id: @id)
+        @question = Question.last
+        @question_id = Question.last.id
+    end
+
 	describe "POST #create" do
-		before(:all) do
-			Room.create(name: "a good room i guess", password: "passwords", roomcode: "aaaa")
-			@id = Room.last.id
-		end
 		it 'puts a question in a room' do
             expect {
                 post :create, :params => {:question => {:body => "a question"}, :room_id => @id}
@@ -13,4 +18,14 @@ RSpec.describe QuestionsController, type: :controller do
             }.to change{Question.all.count}.by(1)
 		end
 	end
+
+    describe "DELETE a question" do
+        it 'deletes a question from the room' do
+            expect {
+                delete :destroy, :params => {id: @question_id, room_id: @id}
+                expect(response).to redirect_to '/rooms/' + @id.to_s
+            }.to change{Question.all.count}.by(-1)
+             .and change{@room.questions.count}.by(-1)
+        end
+    end
 end
